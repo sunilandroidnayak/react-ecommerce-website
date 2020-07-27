18f56a644089a460192635376eadc99b2b5633db
Sunil L <sunilandroidnayak@gmail.com>
2020-07-28 00:15:22 +0530

    Updates

---
+++ precache-manifest.049bf51638b2089779e9fc2a5215ac6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99c131dac10e25c3ec502a04371a80a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eaadabda38ff083d36bb",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5155fd34bd967c028fd7",
+    "url": "/static/js/2.8d5c0cfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8d5c0cfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaadabda38ff083d36bb",
+    "url": "/static/js/main.24402926.chunk.js"
+  },
+  {
+    "revision": "ea460b61a466dd560a5b",
+    "url": "/static/js/runtime-main.21838e15.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
