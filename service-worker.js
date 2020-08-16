e0fff7d574a177aa6f9ab3aea8cdd82eb899b699
Sunil L <sunilandroidnayak@gmail.com>
2020-08-16 15:40:41 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-ecommerce-website/precache-manifest.d15bc5681cef713cfe436f8e1088c492.js"
+  "/react-ecommerce-website/precache-manifest.125314f4d5f00e5d3dc2670d93e3baa3.js"
 );
 
 self.addEventListener('message', (event) => {
